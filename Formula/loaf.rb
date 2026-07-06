6fdd3cd5a309c62bde60c3de30b841d1f99cf104
Levi Figueira <me@levifig.com>
2026-07-06 20:01:17 +0100

    fix: satisfy loaf formula audit

--- Formula/loaf.rb
+++ Formula/loaf.rb
@@ -1,27 +1,26 @@
 class Loaf < Formula
   desc "Opinionated agentic framework for AI coding assistants"
   homepage "https://github.com/levifig/loaf"
-  version "2.0.0-alpha.4"
   license "MIT"
 
   depends_on "git"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/levifig/loaf/releases/download/v#{version}/loaf_#{version}_darwin-arm64.tar.gz"
+      url "https://github.com/levifig/loaf/releases/download/v2.0.0-alpha.4/loaf_2.0.0-alpha.4_darwin-arm64.tar.gz"
       sha256 "d618694790282fd4f1b510262441c35373c8e02d115bf7e3db76ad35a593cde7"
     else
-      url "https://github.com/levifig/loaf/releases/download/v#{version}/loaf_#{version}_darwin-x64.tar.gz"
+      url "https://github.com/levifig/loaf/releases/download/v2.0.0-alpha.4/loaf_2.0.0-alpha.4_darwin-x64.tar.gz"
       sha256 "63f68764c44e6d23747cce6654a344e41069e07b90dc7b00a4a2e42b2f5ca2dc"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/levifig/loaf/releases/download/v#{version}/loaf_#{version}_linux-arm64.tar.gz"
+      url "https://github.com/levifig/loaf/releases/download/v2.0.0-alpha.4/loaf_2.0.0-alpha.4_linux-arm64.tar.gz"
       sha256 "f6e58df38bf91ebb4d148105d82087ffd3791a684b19ad746d95a57f6e389c76"
     else
-      url "https://github.com/levifig/loaf/releases/download/v#{version}/loaf_#{version}_linux-x64.tar.gz"
+      url "https://github.com/levifig/loaf/releases/download/v2.0.0-alpha.4/loaf_2.0.0-alpha.4_linux-x64.tar.gz"
       sha256 "35a5960664d8b541499a989da85866fa28b818c726a1c9264aaa47a3c0f52430"
     end
   end
